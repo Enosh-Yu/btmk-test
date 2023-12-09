d5911ed6b6cb403882b601b054348066d13e4425
Loiskjs <151837800+Loiskjs@users.noreply.github.com>
2023-12-09 09:12:23 +0900

    page3.js

--- antd-demo/src/app/page.js
+++ antd-demo/src/app/page.js
@@ -1,16 +1,37 @@
 'use client'
 
 import React from 'react'
-import { Button, ConfigProvider } from 'antd'
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-import theme from './theme/themeConfig.js'
+const { Header, Content, Footer } = Layout;
 
 const HomePage = () => (
-  <ConfigProvider theme = {theme}>
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
-  </ConfigProvider>
+  <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©2018 Created by Ant UED
+    </Footer>
+  </Layout>
 );
 
 export default HomePage;
