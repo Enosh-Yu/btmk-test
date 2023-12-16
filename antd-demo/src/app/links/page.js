2e27d127f4bb3f2df80afc5b8f1572385b3e11dd
Loiskjs <151837800+Loiskjs@users.noreply.github.com>
2023-12-16 18:56:01 +0900

    lsmradio/ 세부항목 수정

--- antd-demo/src/app/links/page.js
+++ antd-demo/src/app/links/page.js
@@ -11,37 +11,37 @@ import { HomeOutlined, PlayCircleFilled, LinkOutlined, TranslationOutlined } fro
 const data = [
   {
     href: 'http://www.lsm.org',
-    title: <p style={{color:'brown'}}>'About LSM — www.lsm.org'</p>,
+    title: <p style={{color:'brown'}}>About LSM — www.lsm.org</p>,
     description: "Living Stream Ministry is a non-profit corporation that is primarily dedicated to the publication of the ministry of Watchman Nee and Witness Lee. Living Stream Ministry has one goal: to supply spiritual food for the nourishment of all believers that they may grow in the divine life for the sake of the building up of the Body of Christ."
   },
   {
     href: 'http://www.livingstream.com',
-    title: <p style={{color:'brown'}}>'Books — www.livingstream.com'</p>,
+    title: <p style={{color:'brown'}}>Books — www.livingstream.com</p>,
     description: "Living Stream Ministry publishes the works of Watchman Nee and Witness Lee, providing the authoritative and definitive collections of treasures from these two servants of the Lord Jesus Christ. Their writings focus on the enjoyment of the divine life, which all the believers possess, and on the building up of the church, the goal of God's work with man in this age."
   },
   {
     href: 'http://www.recoveryversion.org',
-    title: <p style={{color:'brown'}}>'The Recovery Version of the Bible — www.recoveryversion.org'</p>,
+    title: <p style={{color:'brown'}}>The Recovery Version of the Bible — www.recoveryversion.org</p>,
     description: "The Recovery Version of the Bible contains numerous study aids, including, the subject and background of each book; detailed, interpretive outlines; enlightening footnotes, valuable cross-references, and a variety of useful charts and maps."
   },
   {
     href: 'http://www.ministrybooks.org',
-    title: <p style={{color:'brown'}}>'Online Publications — www.ministrybooks.org'</p>,
+    title: <p style={{color:'brown'}}>Online Publications — www.ministrybooks.org</p>,
     description: "Living Stream Ministry provides the complete text of many of its ministry publications online in the hope that the riches of Christ can be broadly sown throughout the earth for the benefit of all the Lord's children."
   },
   {
     href: 'http://www.affcrit.com',
-    title: <p style={{color:'brown'}}>'Affirmation & Critique — www.affcrit.com'</p>,  
+    title: <p style={{color:'brown'}}>Affirmation & Critique — www.affcrit.com</p>,  
     description: "Affirmation & Critique addresses crucial aspects of Christian thought and practice in light of a vital, yet neglected, aspect of the Bible—the oikonomia of God (Eph. 3:9). The essential and profound truths of the Bible, which involve God's dispensing of Himself and all that He has into man, are affirmed, and many traditional teachings are responsibly critiqued in light of this economy."
   },
   {
     href: 'http://www.emanna.com',
-    title: <p style={{color:'brown'}}>'eManna — www.emanna.com'</p>,  
+    title: <p style={{color:'brown'}}>eManna — www.emanna.com</p>,  
     description: "eManna delivers the Word of God as the bread of life daily via email. With a few verses from the Bible and a short word of ministry, eManna will nourish and strengthen your faith. This service is free."
   },
   {
     href: 'http://www.lsmwebcast.com',
-    title: <p style={{color:'brown'}}>'Webcast Subscription Service — www.lsmwebcast.com'</p>,
+    title: <p style={{color:'brown'}}>Webcast Subscription Service — www.lsmwebcast.com</p>,
     description: "Living Stream Ministry provides a subscription webcast service that enables subscribers to access the video content of recent conferences and also selected conferences and video trainings by Witness Lee."
   },
 ];
